6320e9b0205e9644870381cded3ba2f76f66a928
Serg <your@serg.com>
2022-06-11 12:56:28 +0500

    Create SettingsController

--- HW15/HW15/App/AppDelegate.swift
+++ HW15/HW15/App/AppDelegate.swift
@@ -15,7 +15,7 @@ var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow()
         
-        window.rootViewController = SettingsViewController()
+        window.rootViewController = SettingsController()
         window.makeKeyAndVisible()
         
         self.window = window
