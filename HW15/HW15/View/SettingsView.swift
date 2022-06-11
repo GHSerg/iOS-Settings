14f9397f1cf2d1c3023d928c4b70c219902dcedb
Serg <your@serg.com>
2022-06-11 12:57:14 +0500

    Create SettingsView

--- HW15/HW15/View/SettingsView.swift
+++ HW15/HW15/View/SettingsView.swift
@@ -4,3 +4,22 @@
 //
 //  Created by Sergey Bodnar on 11.06.2022.
 //
+
+import UIKit
+
+final class SettingsView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = .red
+    }
+}
