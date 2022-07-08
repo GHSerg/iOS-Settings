eade0591301fe979c448d8c935acb0093fafc959
Serg <your@serg.com>
2022-07-08 13:53:32 +0500

    Add file readme

--- HW15/HW15/App/AppDelegate.swift
+++ HW15/HW15/App/AppDelegate.swift
@@ -12,7 +12,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let navigationController = UINavigationController.init(rootViewController: SettingsController())
         window = UIWindow(frame: UIScreen.main.bounds)
---
+++ readme.md
@@ -0,0 +1,20 @@
+# iOS-Settings
+ 
+<img width="525" alt="4" src="https://user-images.githubusercontent.com/78469282/172052846-adf389de-a11f-4793-b71e-97df4a1b5b23.pn">
+
+# Описание
+Стандартный экран настроек. При нажатии на ячейку в консоль выводится заголовок ячейк.
+Применен паттерн MVC, без Storyboard (интерфейс кодом), Gitflow. 
+
+
+# Языки программирования
+Swift
+
+# Технологии, API/Frameworks, ПО
+ - UIKit, NoStoryboard, Autolayout;    
+ - OOP, POP, DRY, SOLID, MVС;    
+ - Fork, GitFlow; 
+ - Символы SF.
+
+# Оборудование
+MacBook Air (M1, 2020)
